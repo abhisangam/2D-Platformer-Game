9aa57e4e14bb3c3bda6a7cc5ac4c5df938086b5a
abhisangam <abhishekasangam@gmail.com>
2024-04-28 17:06:26 +0530

    Added animations to key

--- Assets/Scripts/CollectibleKey.cs
+++ Assets/Scripts/CollectibleKey.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CollectibleKey : MonoBehaviour
 {
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
             collision.gameObject.GetComponent<PlayerController>().CollectKey();
-            Destroy(gameObject);
+            animator.SetTrigger("KeyCollected");
+            Destroy(gameObject, 2.0f);
         }
     }
 }
